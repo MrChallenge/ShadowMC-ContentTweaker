10fb765f4e04981c1c86df3565932ef58511df47
SHO8814 <69278759+MrChallenge@users.noreply.github.com>
2026-04-12 00:24:58 +0200

    initial commit

--- src/main/java/link/sho8814/core/items/BlockItemModelTest2.java
+++ src/main/java/link/sho8814/core/items/BlockItemModelTest2.java
@@ -9,6 +9,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+
 import java.util.List;
 
 public class BlockItemModelTest2 extends BlockItem {
